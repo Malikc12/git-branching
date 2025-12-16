fa61a53c7291f40bab64f50c8e4b640d28a6d5e6
Malikc12 <canteberryma@gmail.com>
2025-12-16 08:56:38 -0600

    push

--- src/main/java/com/northwind/app.java
+++ src/main/java/com/northwind/app.java
@@ -1,4 +1,15 @@
 package com.northwind;
 
+import java.util.Scanner;
+
 public class app {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+    }
 }
